bd1d45e0448ef9788341ce50f2aa75a68ea25ebb
Roman Beyko <romabeyko@gmail.com>
2022-05-04 21:23:27 +0300

    chore: warning fix [task]: array training
    [Description list]
    refactor: PR cleaning
    
    [Notes]

--- src/array/array_training.rs
+++ src/array/array_training.rs
@@ -1,37 +1,10 @@
 use rand::Rng;
-use std::io::*;
 
-pub fn work_with_array() {
-
-    // let array = array_random(10);      // print random Array
-    // println!("Array: {:?}", array);
-
-    // println!("{:?}", negative_elements(array_random(10))); // print negative elements of Array
-    
-    // println!("{:?}", sum_elements(array_random(10))); // print sum of all elements in Array
-
-    // println!("{:?}", min_max(array_random(10))); // Max and Min elemsnts of Array
-
-    // println!("{:?}", cound_even_and_odd_elements(array_random(10))); // Total mumber even and odd elements in Array
-
-    // println!("{:?}", imput_element(array_random(10))); // Insert element for keybord to Array
-
-    // println!("{}", count_duplicate_in_array(array_random(10))); // Count duplicate elements in Array
-
-    // println!("{:?}", delet_duplicate(array_random(10))); // Delete duplicate elements for Array
-
-    // println!("{:?}", reverse_array_cycle(array_random(8))); // Reverse Array
-    // println!("{:?}", reverse_array_push(array_random(10))); // Reverse push Array
-
-    // println!("{:?}", positive_array_and_negative_array(array_random(10))); // Two Arrays with positive and negative elements
-
-    // println!("{:?}", sort_array_left_and_right(array_random(10))); // Sort Array in ascending and descending
-}
-
+#[warn(dead_code)]
 fn array_random(array_size: u8) -> Vec<i16> {
     array_generation(array_size, -5, 5)
- }
-
+}
+#[warn(dead_code)]
 fn array_generation(array_size: u8, range_start: i16, range_end: i16) -> Vec<i16>{
     let mut array = vec![];
 
@@ -42,6 +15,7 @@ fn array_generation(array_size: u8, range_start: i16, range_end: i16) -> Vec<i16
     return array;
 }
 
+#[warn(dead_code)]
 fn negative_elements(arr: Vec<i16>) -> Vec<i16> {
     let mut negative_array = vec![];
     for n in 0..arr.len() {
@@ -52,6 +26,7 @@ fn negative_elements(arr: Vec<i16>) -> Vec<i16> {
     return negative_array;
 }
 
+#[warn(dead_code)]
 fn sum_elements(arr: Vec<i16>) -> i16 {
     let mut sum: i16 = 0;
     for n in 0..arr.len() {
@@ -60,6 +35,7 @@ fn sum_elements(arr: Vec<i16>) -> i16 {
     return sum;
 }
 
+#[warn(dead_code)]
 fn min_max(arr: Vec<i16>) -> (i16, i16){
     let mut max = arr[0];
     let mut min = arr[0];
@@ -74,6 +50,7 @@ fn min_max(arr: Vec<i16>) -> (i16, i16){
     return (min, max);
 }
 
+#[warn(dead_code)]
 fn cound_even_and_odd_elements(arr: Vec<i16>) -> (i16, i16) {
     let mut number_even = 0;
     let mut number_odd = 0;
@@ -87,35 +64,7 @@ fn cound_even_and_odd_elements(arr: Vec<i16>) -> (i16, i16) {
     return (number_even, number_odd);
 }
 
-fn imput_element(arr: Vec<i16>) -> Vec<i16> {
-    println!("*Input element input: {:?}", arr);
-    println!("Enter number to push in the Array between -1000 and 1000:");
-    let mut temp_arr = arr;
-    let the_limits_of_the_entered_number = 1000;
-
-    let input_number = imput_symbol(the_limits_of_the_entered_number);
-    temp_arr.push(input_number as i16);
-    return temp_arr;
-}
-fn imput_symbol(limit: i32) -> i32 {
-    let mut symbol = String::new();
-    stdin().read_line(&mut symbol).expect("Failed to read line");
-
-    let input_number: i32 = match symbol.trim().parse(){
-        Ok(num) => num,
-        Err(_) => {
-            println!("You enter symbol. Please enter a number:");
-            return imput_symbol(limit);
-        }
-    };
-    if input_number >= -limit && input_number <= limit {   
-        return input_number;
-    }else {
-        println!("Please enter a number between {} and {}:", -limit, limit);
-        return imput_symbol(limit);
-    }
-}
-
+#[warn(dead_code)]
 fn count_duplicate_in_array(arr: Vec<i16>) -> i16{
     let mut count_duplicate = 0;
     let mut temp_arr = arr.clone();
@@ -130,6 +79,7 @@ fn count_duplicate_in_array(arr: Vec<i16>) -> i16{
     return count_duplicate;
 }
 
+#[warn(dead_code)]
 fn delet_duplicate(arr: Vec<i16>) -> Vec<i16>{
     let mut temp_arr = arr.clone();
     let mut unique_elements_array = vec![];
@@ -145,6 +95,7 @@ fn delet_duplicate(arr: Vec<i16>) -> Vec<i16>{
  return unique_elements_array;
 }
 
+#[warn(dead_code)]
 fn reverse_array_cycle(arr: Vec<i16>) -> Vec<i16> {
     let mut revers_arr = arr.clone();
     let leng = revers_arr.len()-1;
@@ -154,6 +105,7 @@ fn reverse_array_cycle(arr: Vec<i16>) -> Vec<i16> {
     return revers_arr;
 }
 
+#[warn(dead_code)]
 fn reverse_array_push(arr: Vec<i16>) -> Vec<i16> {
     let temp_arr = arr;
     let mut revers_push_arr: Vec<i16> = vec![];
@@ -164,6 +116,7 @@ fn reverse_array_push(arr: Vec<i16>) -> Vec<i16> {
     return revers_push_arr;
 }
 
+#[warn(dead_code)]
 fn positive_array_and_negative_array(arr: Vec<i16>) -> (Vec<i16>, Vec<i16>) {
     let mut positive_arr: Vec<i16> = vec![];
     let mut negative_arr: Vec<i16> = vec![];
@@ -177,6 +130,7 @@ fn positive_array_and_negative_array(arr: Vec<i16>) -> (Vec<i16>, Vec<i16>) {
 return (positive_arr, negative_arr);
 }
 
+#[warn(dead_code)]
 fn sort_array_left_and_right(arr: Vec<i16>) -> (Vec<i16>, Vec<i16>) {
     let mut temp_arr_left = arr;
     for j in 1..temp_arr_left.len() {
